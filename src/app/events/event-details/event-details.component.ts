eb33f04d333be87651c45ca0998a91f74121be29
Nwachukwu Ejiofor <nwachukwu.ejiofor@xputertechnologies.com>
2020-01-23 12:20:18 +0100

    Filtering and sorting sessions data

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -12,6 +12,8 @@ export class EventDetailsComponent implements OnInit {
 
   event: IEvent;
   addMode: boolean;
+  filterBy = 'all';
+  sortBy = 'votes';
 
   constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
--- src/app/events/sessions-list/sessions-list.component.ts
+++ src/app/events/sessions-list/sessions-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ISession } from './../../shared/event.model';
 
 @Component({
@@ -6,11 +6,45 @@ import { ISession } from './../../shared/event.model';
   templateUrl: './sessions-list.component.html',
   styleUrls: ['./sessions-list.component.css']
 })
-export class SessionsListComponent implements OnInit {
+export class SessionsListComponent implements OnInit, OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  @Input() sortBy: string;
+  visibleSessions: ISession[];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+      this.sortBy === 'votes' ? this.visibleSessions.sort(sortByVotesDesc) : this.visibleSessions.sort(sortByNameAsc);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) {
+    return 1;
+  } else if (s1.name === s2.name) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
